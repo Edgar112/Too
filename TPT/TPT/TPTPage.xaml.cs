93ddc6817346a4c2f91340a3a7a992d8193e6ec3
Edgar112 <edgar4443@live.com>
2018-05-10 11:22:53 +0300

    final fix

--- TPT/TPT/TPTPage.xaml.cs
+++ TPT/TPT/TPTPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿
 using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace TPT
 {
@@ -8,6 +11,8 @@ namespace TPT
         public TPTPage()
         {
             InitializeComponent();
+
+            AppCenter.Start("android=0bc8c2ed-9d37-432b-8c87-145f00f05dd4;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
         }
     }
 }
